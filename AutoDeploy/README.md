87a634133163104fea2e1137bd87b7fbcf8eeb26
rajeshcool16 <rajeshcool16@gmail.com>
2017-10-16 18:27:13 +0530

    To fix the issue

--- AutoDeploy/README.md
+++ AutoDeploy/README.md
@@ -1,10 +1,3 @@
-﻿UNZIP AND AUTO DEPLOY
+﻿# Unzip1
 
-Simple module to Auto Deploy Html and dependent files.
-
-Upload compressed .zip/.rar file.
-Enter the path for the files to be extracted.
-
-The contents in the compressed file is extracted and deployed to the specified path.
-These files are declared as static hence can be accessed via browser.
 
--- AutoDeploy/server.js
+++ AutoDeploy/server.js
@@ -14,10 +14,61 @@ var unzip = require('unzip');
 var fileUpload = require('express-fileupload');
 var mkdirp = require('mkdirp');
 
-app.use(bodyParser.urlencoded({ extended: false }));
+//app.use(bodyParser.urlencoded({ extended: false }));
 
 var path = require('path');
 
+var multipart = require('connect-multiparty');
+var multipartMiddleware = multipart();
+//var multer = require('multer');
+//var autoReap = require('multer-autoreap');
+
+//To extract the uploaded zip file
+app.post('/upload', multipartMiddleware, function (req, res) {
+    var actualpath = req.files.zipFile.path;
+
+    var extractPath = req.body.extractPath;
+
+    //Declare the path as static to access files from browser
+    app.use(express.static('public'));
+    app.use(express.static('Html'));
+    app.use('/' + extractPath, express.static(extractPath));
+    var readChunk = require('read-chunk');
+    var fileType = require('file-type');
+
+    if (!req.files)
+        return res.status(400).send('No files were uploaded.');
+
+    var zipFileName = req.files.zipFile.name;
+    var fName = zipFileName.substring(0, zipFileName.indexOf("."));
+
+    var ftype = fileType(readChunk.sync(actualpath, 0, 4100));
+    if (ftype.ext === 'zip' || ftype.ext === 'rar') {
+        //Extract if Path/Directory already exists
+        if (fs.existsSync(extractPath)) {
+            fs.createReadStream(actualpath).pipe(unzip.Extract({ path: extractPath }));
+            res.send('<p>Directory Exists: Extracted Successfully.</p> <p> Path:- http://localhost:' + port + '\/' + extractPath + '\/' + fName + '\/..</p>');
+        }
+        else {
+            //Create Path/Directory and then Extract
+            mkdirp(extractPath, function (err) {
+                if (err)
+                    res.send(err);
+                else {
+                    fs.createReadStream(actualpath).pipe(unzip.Extract({ path: extractPath }));
+                    res.send('<p>Directory Created: Extracted Successfully.</p> <p> Path:- http://localhost:' + port + '\/' + extractPath + '\/' + fName + '\/..</p>');
+                }
+            });
+        }
+    }
+    else {
+        res.send('<p>Please upload zipped files with extension .zip or .rar</p>');
+    }
+
+    res.on('finish', function () {
+        fs.unlink(actualpath);
+    });
+});
 
 
 //Page to enter Path, Upload File and Submit.
@@ -26,47 +77,7 @@ app.get('/', function (req, res) {
 });
 
 app.use(fileUpload());
-//Post upload
-app.post('/upload', function (req, res) {
-    var extractPath = req.body.extractPath;
 
-    //Declare the path as static to access files from browser
-    app.use(express.static('public'));
-    app.use(express.static('Html'));
-    app.use('/' + extractPath,express.static(extractPath));
-    var readChunk = require('read-chunk');
-    var fileType = require('file-type');
-
-    if (!req.files)
-        return res.status(400).send('No files were uploaded.');
-    
-    var zipFileName = req.files.zipFile.name;
-    //var buffer = 
-    var ftype = fileType(readChunk.sync(path.resolve(zipFileName), 0, 4100));
-    if (ftype.ext === 'zip' || ftype.ext === 'rar') {
-        //Extract if Path/Directory already exists
-        if (fs.existsSync(extractPath)) {
-            fs.createReadStream(path.resolve(zipFileName)).pipe(unzip.Extract({ path: extractPath }));
-            res.send('<p>Directory Exists: Extracted Successfully.</p> <p> Path:- http://localhost:' + port + '\/' + extractPath + '..</p>');
-
-        }
-        else {
-            //Create Path/Directory and then Extract
-            mkdirp(extractPath, function (err) {
-                if (err)
-                    res.send(err);
-                else {
-                    fs.createReadStream(path.resolve(zipFileName)).pipe(unzip.Extract({ path: extractPath }));
-                    res.send('<p>Directory Created: Extracted Successfully.</p> <p> Path:- http://localhost:' + port + '\/' + extractPath + '..</p>');
-
-                }
-            });
-        }
-    }
-    else {
-        res.send('<p>Please upload zipped files with extension .zip or .rar</p>');
-    }
-});
 var server = app.listen(port, function () {
-    console.log('Node server is running..');
+    console.log('Node server is running on' + port);
 });
